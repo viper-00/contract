0b0854f8f07cf89a3499493d05ec973dc18fa72b
viper-00 <zhongmingyang2000@gmail.com>
2022-09-04 00:14:14 +0800

    update contract

--- zombie/zombiefactory.sol
+++ zombie/zombiefactory.sol
@@ -1,39 +1,44 @@
-// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.19;
 
-pragma solidity ^0.8.0;
+import "./ownable.sol";
 
-contract ZombieFactory {
-    event NewZombie(uint256 zombieId, string name, uint256 dna);
+contract ZombieFactory is Ownable {
 
-    uint256 dnaDigits = 16;
-    uint256 dnaModulus = 10**dnaDigits;
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint256 dna;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
     }
 
     Zombie[] public zombies;
 
-    mapping(uint256 => address) public zombieToOwner;
-    mapping(address => uint256) ownerZombieCount;
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint256 _dna) internal {
-        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string _name, uint _dna) internal {
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint256) {
-        uint256 rand = uint256(keccak256(_str));
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
-        uint256 randDna = _generateRandomDna(_name);
-        randDna = randDna - (randDna % 100);
+        uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
+
 }
--- zombie/zombiefeeding.sol
+++ zombie/zombiefeeding.sol
@@ -1,52 +1,58 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
 contract KittyInterface {
-    function getKitty(uint256 _id)
-        external
-        view
-        returns (
-            bool isGestating,
-            bool isReady,
-            uint256 cooldownIndex,
-            uint256 nextActionAt,
-            uint256 siringWithId,
-            uint256 birthTime,
-            uint256 matronId,
-            uint256 sireId,
-            uint256 generation,
-            uint256 genes
-        );
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
 }
 
 contract ZombieFeeding is ZombieFactory {
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    // 这里修改函数定义
-    function feedAndMultiply(
-        uint256 _zombieId,
-        uint256 _targetDna,
-        string _species
-    ) public {
-        require(msg.sender == zombieToOwner[_zombieId]);
-        Zombie storage myZombie = zombies[_zombieId];
-        _targetDna = _targetDna % dnaModulus;
-        uint256 newDna = (myZombie.dna + _targetDna) / 2;
-        // 这里增加一个 if 语句
-        if (keccak256(_species) == keccak256("kitty")) {
-            newDna = newDna - (newDna % 100) + 99;
-        }
-        _createZombie("NoName", newDna);
-    }
+  KittyInterface kittyContract;
 
-    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
-        uint256 kittyDna;
-        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
-        // 并修改函数调用
-        feedAndMultiply(_zombieId, kittyDna, "kitty");
+  modifier ownerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
+
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
     }
+    _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
 }
